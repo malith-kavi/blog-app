ba3c54c7fc414bc2f32e917f5b108bc565d01047
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-09-27 02:27:54 +0530

    updates

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT ||8000;
+const PORT = process.env.PORT || 8000;
+
+//initializw
+app.use(express.json({extended:false}));
 
 //just a test route for now 
 app.get("/", (req, res) => res.send("Hello,World!"));
+app.post("/", (req, res) => res.send(`Hello, ${req.body.name}`));
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
