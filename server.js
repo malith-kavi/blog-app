7cfdbcc3ca928963f3214019b079684a24fc44d7
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-02 01:29:27 +0530

    updates on server

--- server.js
+++ server.js
@@ -2,11 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const articlesInfo = {
+    "learn-react": {
+        comments: [],
+    },
+    "learn-node": {
+        comments: [],
+    },
+    "learn-thought-on-learning-react": {
+        comments: [],
+    },
+}
+
 //initializw
 app.use(express.json({extended:false}));
 
-//just a test route for now 
-app.get("/", (req, res) => res.send("Hello,World!"));
-app.post("/", (req, res) => res.send(`Hello, ${req.body.name}`));
+app.post('/api/articles/:name/add-comments', (req, res) => {
+    const{username, text} =req.body
+    const articleName =req.params.name;
+    articlesInfo[articleName].comments.push({username,text});
+    res.status(200).send(articlesInfo[articleName]);
+});
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+
